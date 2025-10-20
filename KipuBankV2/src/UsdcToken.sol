4782d0496fe18bc504d411ea793679237dc2a5f0
agustin mondelli <agustinmondelli@hotmail.com>
2025-10-20 15:58:48 -0300

    .

---
+++ KipuBankV2/src/UsdcToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract usdcToken is ERC20 {
+
+    constructor() ERC20("USD Coin", "USDC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+
+}
+
